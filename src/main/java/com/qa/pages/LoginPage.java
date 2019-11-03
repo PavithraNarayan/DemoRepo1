9efd7c4cf5e1cd24d5ead6ce9fecd1572b5d2ebc
PavithraNarayan <57241651+PavithraNarayan@users.noreply.github.com>
2019-11-03 03:22:34 -0800

    Added Search page & Modified Login Page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,4 +2,12 @@ package com.qa.pages;
 
 public class LoginPage {
 
+	public void loginPage() {
+		System.out.println("Login Page");
+		
+	}
+	public void setLogin() {
+		System.out.println("set login");
+	
+	}
 }
